40a128b6b545ac76a2d2c79816c7acb780018db4
Rachit Dubey <rachitricky777@gmail.com>
2026-02-09 09:23:04 +0530

    LDR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <Arduino.h>
+#define ldrPin A0 // LDR connected to analog pin A0
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    pinMode(ldrPin, INPUT);
+
 }
 
 void loop() {
 // write your code here
+    int ldrValue = analogRead(ldrPin);
+    int lightPercent = map(ldrValue, 0, 1023, 100, 0); // Map the LDR value to a percentage (0-100)
+    Serial.println(ldrValue);
+    Serial.println(lightPercent);
+    delay(300); // Delay for 300 milliseconds before the next reading
 }
